c193664e6f0455b0000c69f711cfa24be35ebaae
dingfei <dingfei@jovision.com>
2019-10-12 15:10:22 +0800

    add:composer package done

---
+++ src/NumberPlus.php
@@ -0,0 +1,8 @@
+<?php
+namespace Cmtools;
+class NumberPlus{
+  public static function plus($num1,$num2){
+    return $num1 + $num2;
+  }
+}
+?>
---
+++ tests/NumberPlusTest.php
@@ -0,0 +1,5 @@
+<?php
+require_once "../vendor/autoload.php";
+use Cmtools\NumberPlus;
+var_dump(NumberPlus::plus(1,2));
+?>
